1f6c5e164b4f328f510f476039b94b9bbe5565ed
AbelStuff <73490542+Appel-man@users.noreply.github.com>
2024-12-05 14:43:14 +0000

    Changes

--- js/map.js
+++ js/map.js
@@ -1,9 +1,7 @@
 var map = L.map('map').setView([51.21331029419274, 4.258141283951909], 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    minZoom: 2, // Minimum zoom level (usually 0 or 1)
-    maxZoom: 18
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     
     L.marker([51.21331029419274, 4.258141283951909]).addTo(map)
